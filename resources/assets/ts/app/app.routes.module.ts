9d5685b18d8a2cb9b1f215109549bc0347adea0d
otto sandstrom <otto.sandstrom@swebro.eu>
2017-11-02 17:34:53 -0700

    imports

--- resources/assets/ts/app/app.routes.module.ts
+++ resources/assets/ts/app/app.routes.module.ts
@@ -1,15 +1,18 @@
 import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
 
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {PageNotFoundComponent} from "./core/containers/page-not-found.component";
-import {NgModule} from "@angular/core";
+
 import {EventsModule} from "./events/events.module";
 import {SignUpsModule} from "./sign_ups/sign-ups.module";
+import {UsersModule} from "./users/users.module";
 
 const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     {path: 'dashboard', component: DashboardComponent},
     {path: 'events', loadChildren: () => EventsModule},
+    {path: 'users', loadChildren: () => UsersModule},
     {path: 'signUp', loadChildren: () => SignUpsModule},
     {path: '**', component: PageNotFoundComponent}
 ];
